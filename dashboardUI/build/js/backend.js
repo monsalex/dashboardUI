7bafc33182bced250ad1c62e31df2ef3c5a972d0
Alejandro Monsalvo <monsalex@MacBook-Pro-de-Alejandro.local>
2020-03-03 11:06:12 -0600

    SE agrego la nueva seccion de efectivo y sus graficas

--- dashboardUI/build/js/backend.js
+++ dashboardUI/build/js/backend.js
@@ -73,8 +73,86 @@ function pickDateBackend(startdate, enddate, source){
             fillReporteOrdenes(data.ordersByWeek);
 
             fillTazBines(data.ordersByPayBines);
+
+            fillCash(data);
         }
     })
+}
+
+function fillCash(data){
+
+    if ($('#mainb_cash').length) {
+
+        var echartBar = echarts.init(document.getElementById('mainb_cash'), theme);
+
+        echartBar.setOption({
+            title: {
+                text: '',//'Graph title',
+                subtext: ''//'Graph Sub-text'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data: ['Pagadas', 'No pagadas']
+            },
+            toolbox: {
+                show: false
+            },
+            calculable: false,
+            xAxis: [{
+                type: 'category',
+                data: ['1?', '2?', '3?', '4?', '5?', '6?', '7?', '8?', '9?', '10?', '11?', '12?']
+            }],
+            yAxis: [{
+                type: 'value'
+            }],
+            series: [{
+                name: 'Pagadas',
+                type: 'bar',
+                data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
+                markPoint: {
+                    data: [{
+                        type: 'max',
+                        name: '???'
+                    }, {
+                        type: 'min',
+                        name: '???'
+                    }]
+                },
+                markLine: {
+                    data: [{
+                        type: 'average',
+                        name: '???'
+                    }]
+                }
+            }, {
+                name: 'No pagadas',
+                type: 'bar',
+                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+                markPoint: {
+                    data: [{
+                        name: 'sales',
+                        value: 182.2,
+                        xAxis: 7,
+                        yAxis: 183,
+                    }, {
+                        name: 'purchases',
+                        value: 2.3,
+                        xAxis: 11,
+                        yAxis: 3
+                    }]
+                },
+                markLine: {
+                    data: [{
+                        type: 'average',
+                        name: '???'
+                    }]
+                }
+            }]
+        });
+
+    }
 }
 
 function fillTazBines(data){
