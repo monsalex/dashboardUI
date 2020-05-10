f88cae6ff2e3c1a72e44143cd559ec89fe296165
Monsanto Alexander <e_pecas@hotmail.com>
2020-05-10 01:13:58 -0500

    Decargar de archivo CSV

---
+++ dashboardUI/build/js/backendFile.js
@@ -0,0 +1,11 @@
+$('#btnCaracteristicas').click(function(e){
+    e.preventDefault();  //stop the browser from following
+    window.location.href = "../../files/Caracteristicas.csv";
+    
+});
+
+$('#btnEspecificaciones').click(function(e){
+    e.preventDefault();  //stop the browser from following
+    window.location.href = "../../files/Especificaciones.csv";
+    
+});
